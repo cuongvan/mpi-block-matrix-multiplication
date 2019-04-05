08b4181bd3e0bdea398f4a25097a91c0e73f4ff6
Cuong Van <cuongvtien@gmail.com>
2019-04-06 01:39:16 +0700

    run ok

--- test_send_submatrix.c
+++ test_send_submatrix.c
@@ -2,49 +2,76 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-const int N = 10;
-double **a, **tmp;
+const int N = 600;
+double *a, *b, *c;
 
-double** matrix_alloc(int nrows, int ncols) {
-    double** matrix;
-    int i;
-    matrix = (double**) malloc(nrows * sizeof(double *));
-    for (i = 0; i < nrows; i++)
-        matrix[i] = (double*) malloc(ncols * sizeof(double));
-    return matrix;
+void alloc_matrices() {
+    a = (double*) malloc(N * N * sizeof(double));
+    b = (double*) malloc(N * N * sizeof(double));
+    c = (double*) malloc(N * N * sizeof(double));
 }
 
-void init_matrix(double** matrix, int nrows, int ncols) {
+void init_matrices() {
     int i, j;
-    for (i = 0; i < nrows; i++) {
-        for (int j = 0; j < nrows; j++)
-            matrix[i][j] = i + j;
+    for (i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            a[i*N + j] = j + 1;
+            b[i*N + j] = j + 1;
+            c[i*N + j] = 0;
+        }
+            
     }
 }
 
-void print_matrix(double** matrix, int nrows, int ncols) {
+void free_matrices() {
+    free(a);
+    free(b);
+    free(c);
+}
+
+void print_matrix(double* matrix) {
     int i, j;
-    for (i = 0; i < nrows; i++) {
-        for (int j = 0; j < nrows; j++)
-            printf("% 6.0lf ", matrix[i][j]);
+    for (i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++)
+            printf("% 6.0lf ", matrix[i*N + j]);
         printf("\n");
     }
+    printf("-------------------------------------------------\n");
 }
 
 MPI_Datatype create_block_type(int row, int col, int sub_size) {
     MPI_Datatype new_type;
 
-    int starts = {row, col};
-    int big_sizes = {N, N};
-    int sub_sizes = {sub_size, sub_size};
-    MPI_Type_create_subarray(2, big_sizes, sub_size, starts, MPI_ORDER_C, MPI_INT, &new_type);
-    MPI_Type_commit(new_type);
+    int starts[2] = {row, col};
+    int big_sizes[2] = {N, N};
+    int sub_sizes[2] = {sub_size, sub_size};
+    MPI_Type_create_subarray(2, big_sizes, sub_sizes, starts, MPI_ORDER_C, MPI_INT, &new_type);
+    MPI_Type_commit(&new_type);
 
     return new_type;
 }
 
-MPI_Datatype create_vector_type(int nrows, int ncols) {
+void mm(int crow, int ccol, /* Corner of C block */
+        int arow, int acol, /* Corner of A block */
+        int brow, int bcol, /* Corner of B block */
+        int block_size)     /* Block size */
+{
+    int i, j, k;
+    double *aptr, *bptr, *cptr;
+
+    for (i = 0; i < block_size; i++)
+        for (j = 0; j < block_size; j++) {
+            cptr = c + (crow + i)*N + ccol + j;
+            aptr = a + (arow + i)*N + acol;
+            bptr = b + brow * N + bcol + j;
+            for (k = 0; k < block_size; k++) {
+                *cptr += *(aptr++) * *bptr;
+                bptr += N;
+            }
+        }
 }
+
+void matrix_mul();
 
 int main() {
     int world_size, world_rank;
@@ -53,19 +80,46 @@ int main() {
     MPI_Comm_size(MPI_COMM_WORLD, &world_size);
     MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
 
+    if (world_size != 4) {
+        if (world_rank == 0) {
+            printf("Need 4 processes");
+            MPI_Abort(MPI_COMM_WORLD, 1);
+        }
+        MPI_Finalize();
+        exit(1);
+    }
+
+    alloc_matrices();
+    init_matrices();
+
+    MPI_Barrier(MPI_COMM_WORLD);
+    double time = - MPI_Wtime();
+
+    int crow = (N/2) * (world_rank / 2);
+    int ccol = (N/2) * (world_rank % 2);
+    mm(crow, ccol, crow, 0, 0, ccol, N/2);
+    mm(crow, ccol, crow, N/2, N/2, ccol, N/2);
+
+    // MPI_Reduce(c, c, N*N, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
+    double* tmp;
+    if (world_rank == 0)
+        tmp = (double*) malloc(N * N * sizeof(double));
+    MPI_Reduce(c, tmp, N*N, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
+
+    
     if (world_rank == 0) {
-        a = matrix_alloc(N, N);
-        init_matrix(a, N, N);
-        // print_matrix(a, N, N);
-        MPI_Datatype top_left_block = create_block_type(0, 0, N / 2);
-        MPI_Recv(&a[5][5], 1, top_left_block, MPI_ANY_SOURCE, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-    }
-    else {
-        tmp = matrix_alloc(N/2, N/2);
-        // MPI_Send(tmp, )
+        free(c);
+        c = tmp;
+        tmp = NULL;
     }
 
+    time += MPI_Wtime();
 
-
+    if (world_rank == 0) {
+        // print_matrix(c);
+        printf("Elapsed time: %.2lf seconds\n", time);
+    }
+    
+    free_matrices();
     MPI_Finalize();
 }

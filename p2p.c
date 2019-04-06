e0166f97721be11027fd37d984b0f32232d246b9
Cuong Van <cuongvtien@gmail.com>
2019-04-06 12:55:55 +0700

    P2P run ok

--- p2p.c
+++ p2p.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-const int N = 1000;                     /* Square matrix size */
+const int N = 10;                     /* Square matrix size */
 double *a, *b, *c;                      /* Data blocks init in root */
 double *a_block, *b_block, *c_block;    /* Blocks to calculate on each process */
 
@@ -14,7 +14,7 @@ void init_matrix(double* matrix, int size) {
     int i, j;
     for (i = 0; i < size; i++) {
         for (int j = 0; j < size; j++) {
-            matrix[i*size + j] = j + 1;
+            matrix[i*size + j] = 2*(i/(size/2)) + j/(size/2);
         }
     }
 }
@@ -58,6 +58,8 @@ void block_multiply(int block_size) {
 
 int main() {
     int world_size, world_rank;
+    const int block_size = N / 2;
+
 
     MPI_Init(NULL, NULL);
     MPI_Comm_size(MPI_COMM_WORLD, &world_size);
@@ -76,25 +78,28 @@ int main() {
     if (world_rank == 0) {
         a = alloc_matrix(N);
         b = alloc_matrix(N);
+        c = alloc_matrix(N);
         init_matrix(a, N);
         init_matrix(b, N);
+        fill_matrix(c, N, 0);
+        print_matrix(a, N);
     }
 
     /* Start of parallel section */
     MPI_Barrier(MPI_COMM_WORLD);
     double time = - MPI_Wtime();
 
-    a_block = alloc_matrix(N / 2);
-    b_block = alloc_matrix(N / 2);
-    c_block = alloc_matrix(N / 2);
-    fill_matrix(a_block, N/2, 0);
-    fill_matrix(b_block, N/2, 0);
-    fill_matrix(c_block, N/2, 0);
+    a_block = alloc_matrix(block_size);
+    b_block = alloc_matrix(block_size);
+    c_block = alloc_matrix(block_size);
+    fill_matrix(a_block, block_size, 0);
+    fill_matrix(b_block, block_size, 0);
+    fill_matrix(c_block, block_size, 0);
 
     MPI_Datatype array_block;
     if (world_rank == 0) {
         int sizes[2] = {N, N};
-        int subsizes[2] = {N/2, N/2};
+        int subsizes[2] = {block_size, block_size};
         int starts[2] = {0, 0};
         MPI_Type_create_subarray(2, sizes, subsizes, starts,
             MPI_ORDER_C, MPI_DOUBLE, &array_block);
@@ -103,9 +108,6 @@ int main() {
         MPI_Type_size(MPI_DOUBLE, &double_size);
         MPI_Type_create_resized(array_block, 0, 1*double_size, &array_block);
         MPI_Type_commit(&array_block);
-
-        c = alloc_matrix(N);
-        fill_matrix(c, N, 0);
     }
     
     /********************************************************/
@@ -117,85 +119,175 @@ int main() {
         | 2 | 3 |       | 2 | 3 |
 
     */
-    const int blocks[4] = {
-        0, N/2,
+    const int blocks[4] = {         /* Memory address of top-left corners */
+        0, N/2,                     /*   of blocks 0, 1, 2, 3 */
         N*(N/2), N*(N/2) + N/2
     };
 
-    int send_counts[4] = {1, 1, 1, 1};
-
-
-    /*
-        First pass:
+    /* First pass:
         Proc 0: a0 * b0
         Proc 1: a0 * b1
         Proc 2: a2 * b0
-        Proc 3: a3 * b1
-    */
+        Proc 3: a3 * b1 */
     {
-        /* Send one block of a to each process */
-        int a_blocks_indices[4] = {blocks[0], blocks[0], blocks[2], blocks[2]};
-        MPI_Scatterv(a, send_counts, a_blocks_indices, array_block,
-            a_block, (N/2)*(N/2), MPI_DOUBLE, 0, MPI_COMM_WORLD);
+        const int a_tag = 0, b_tag = 1;
+        if (world_rank == 0) {
+            MPI_Request requests[6];
+            
+            MPI_Isend(&a[blocks[0]], 1, array_block, 1, a_tag, MPI_COMM_WORLD, &requests[0]);     /* a0 --> proc 1 */
+            MPI_Isend(&a[blocks[2]], 1, array_block, 2, a_tag, MPI_COMM_WORLD, &requests[1]);     /* a2 --> proc 2 */
+            MPI_Isend(&a[blocks[2]], 1, array_block, 3, a_tag, MPI_COMM_WORLD, &requests[2]);     /* a2 --> proc 3 */
 
-        /* Send one block of b to each process */
-        int b_block_indices[4] = {blocks[0], blocks[1], blocks[0], blocks[1]};
-        MPI_Scatterv(b, send_counts, b_block_indices, array_block,
-            b_block, (N/2)*(N/2), MPI_DOUBLE, 0, MPI_COMM_WORLD);
+            MPI_Isend(&b[blocks[1]], 1, array_block, 1, b_tag, MPI_COMM_WORLD, &requests[3]);     /* b1 --> proc 1 */
+            MPI_Isend(&b[blocks[0]], 1, array_block, 2, b_tag, MPI_COMM_WORLD, &requests[4]);     /* b0 --> proc 2 */
+            MPI_Isend(&b[blocks[1]], 1, array_block, 3, b_tag, MPI_COMM_WORLD, &requests[5]);     /* b1 --> proc 3 */
+
+
+            /* proc 0: copy a0, b0 --> a_block, b_block */
+            int i, j;
+            for (i = 0; i < block_size; i++) {
+                for (j = 0; j < block_size; j++) {
+                    a_block[i*block_size + j] = a[blocks[0] + i*N + j];
+                    b_block[i*block_size + j] = b[blocks[0] + i*N + j];
+                }
+            }
+
+            MPI_Waitall(6, requests, MPI_STATUSES_IGNORE);
+        }
+        else {
+            MPI_Request requests[2];
+            MPI_Irecv(a_block, block_size * block_size, MPI_DOUBLE, 0, a_tag, MPI_COMM_WORLD, &requests[0]);
+            MPI_Irecv(b_block, block_size * block_size, MPI_DOUBLE, 0, b_tag, MPI_COMM_WORLD, &requests[1]);
+
+            MPI_Waitall(2, requests, MPI_STATUSES_IGNORE);
+        }
     }
 
-    /* Multiply the first pair of blocks */
-    block_multiply(N / 2);
+    block_multiply(block_size);
+    MPI_Barrier(MPI_COMM_WORLD);    // Every slave procs have to be done before `a_block` can be reused
 
-    /*
-        Second pass:
-        Proc 0: a1 * b2
-        Proc 1: a1 * b3
-        Proc 2: a3 * b2
-        Proc 3: a3 * b3
-    */
+    /*  Second pass:
+            Proc 0: a1 * b2
+            Proc 1: a1 * b3
+            Proc 2: a3 * b2
+            Proc 3: a3 * b3 */
     {
-        int a_blocks_indices[4] = {blocks[1], blocks[1], blocks[3], blocks[3]};
-        MPI_Scatterv(a, send_counts, a_blocks_indices, array_block,
-            a_block, (N/2)*(N/2), MPI_DOUBLE, 0, MPI_COMM_WORLD);
+        const int a_tag = 0, b_tag = 1;
+        if (world_rank == 0) {
+            MPI_Request requests[6];
+            
+            MPI_Isend(&a[blocks[1]], 1, array_block, 1, a_tag, MPI_COMM_WORLD, &requests[0]);     /* a0 --> proc 1 */
+            MPI_Isend(&a[blocks[3]], 1, array_block, 2, a_tag, MPI_COMM_WORLD, &requests[1]);     /* a2 --> proc 2 */
+            MPI_Isend(&a[blocks[3]], 1, array_block, 3, a_tag, MPI_COMM_WORLD, &requests[2]);     /* a2 --> proc 3 */
 
-        int b_block_indices[4] = {blocks[2], blocks[3], blocks[2], blocks[3]};
-        MPI_Scatterv(b, send_counts, b_block_indices, array_block,
-            b_block, (N/2)*(N/2), MPI_DOUBLE, 0, MPI_COMM_WORLD);
+            MPI_Isend(&b[blocks[3]], 1, array_block, 1, b_tag, MPI_COMM_WORLD, &requests[3]);     /* b1 --> proc 1 */
+            MPI_Isend(&b[blocks[2]], 1, array_block, 2, b_tag, MPI_COMM_WORLD, &requests[4]);     /* b0 --> proc 2 */
+            MPI_Isend(&b[blocks[3]], 1, array_block, 3, b_tag, MPI_COMM_WORLD, &requests[5]);     /* b1 --> proc 3 */
+
+
+            /* proc 0: copy a1, b2 --> a_block, b_block */
+            int i, j;
+            for (i = 0; i < block_size; i++) {
+                for (j = 0; j < block_size; j++) {
+                    a_block[i*block_size + j] = a[blocks[1] + i*N + j];
+                    b_block[i*block_size + j] = b[blocks[2] + i*N + j];
+                }
+            }
+
+            MPI_Waitall(6, requests, MPI_STATUSES_IGNORE);
+        }
+        else {
+            MPI_Request requests[2];
+            MPI_Irecv(a_block, block_size * block_size, MPI_DOUBLE, 0, a_tag, MPI_COMM_WORLD, &requests[0]);
+            MPI_Irecv(b_block, block_size * block_size, MPI_DOUBLE, 0, b_tag, MPI_COMM_WORLD, &requests[1]);
+
+            MPI_Waitall(2, requests, MPI_STATUSES_IGNORE);
+        }
+    }
+
+    block_multiply(block_size);
+
+    /* Send all result to proc 0 */
+    {
+        const int tag = 0;
+        if (world_rank == 0) {
+            MPI_Request requests[3];
+            MPI_Irecv(&c[blocks[1]], 1, array_block, 1, tag, MPI_COMM_WORLD, &requests[0]);
+            MPI_Irecv(&c[blocks[2]], 1, array_block, 2, tag, MPI_COMM_WORLD, &requests[1]);
+            MPI_Irecv(&c[blocks[3]], 1, array_block, 3, tag, MPI_COMM_WORLD, &requests[3]);
+
+            /* proc 0: copy c_block --> c0 */
+            int i, j;
+            for (i = 0; i < block_size; i++)
+                for (j = 0; j < block_size; j++)
+                    c[blocks[0] + i*N + j] = c_block[i* block_size + j];
+
+            MPI_Waitall(3, requests, MPI_STATUSES_IGNORE);
+        }
+        else {
+            MPI_Send(c_block, block_size * block_size, MPI_DOUBLE, 0, tag, MPI_COMM_WORLD);
+        }
+    }
+
+    if (world_rank == 0) {
+        printf("Done!\n");
+        if (N <= 10)
+            print_matrix(c, N);
     }
     
-    /* Multiply the remaining pair of blocks */
-    block_multiply(N / 2);
+
+    // /* Multiply the first pair of blocks */
+    // block_multiply(N / 2);
+
+    // /*
+    //     Second pass:
+    //     Proc 0: a1 * b2
+    //     Proc 1: a1 * b3
+    //     Proc 2: a3 * b2
+    //     Proc 3: a3 * b3
+    // */
+    // {
+    //     int a_blocks_indices[4] = {blocks[1], blocks[1], blocks[3], blocks[3]};
+    //     MPI_Scatterv(a, send_counts, a_blocks_indices, array_block,
+    //         a_block, (N/2)*(N/2), MPI_DOUBLE, 0, MPI_COMM_WORLD);
+
+    //     int b_block_indices[4] = {blocks[2], blocks[3], blocks[2], blocks[3]};
+    //     MPI_Scatterv(b, send_counts, b_block_indices, array_block,
+    //         b_block, (N/2)*(N/2), MPI_DOUBLE, 0, MPI_COMM_WORLD);
+    // }
+    
+    // /* Multiply the remaining pair of blocks */
+    // block_multiply(N / 2);
 
 
-    /********************************************************/
-    /* Send the results back to process 0 */
-    int recv_counts[4] = {1, 1, 1, 1};
-    MPI_Gatherv(c_block, (N/2)*(N/2), MPI_DOUBLE, 
-        c, recv_counts, blocks, array_block, 0, MPI_COMM_WORLD);
+    // /********************************************************/
+    // /* Send the results back to process 0 */
+    // int recv_counts[4] = {1, 1, 1, 1};
+    // MPI_Gatherv(c_block, (N/2)*(N/2), MPI_DOUBLE, 
+    //     c, recv_counts, blocks, array_block, 0, MPI_COMM_WORLD);
 
-    free(a_block);
-    free(b_block);
-    free(c_block);
+    // free(a_block);
+    // free(b_block);
+    // free(c_block);
 
-    /* End of paralle section */
-    MPI_Barrier(MPI_COMM_WORLD);
-    time += MPI_Wtime();
+    // /* End of paralle section */
+    // MPI_Barrier(MPI_COMM_WORLD);
+    // time += MPI_Wtime();
 
-    if (world_rank == 0) {
-        if (N <= 10) {
-            print_matrix(a, N);
-            print_matrix(c, N);
-        }
-        printf("Elapsed time: %.2lf seconds\n", time);
-    }
+    // if (world_rank == 0) {
+    //     if (N <= 10) {
+    //         print_matrix(a, N);
+    //         print_matrix(c, N);
+    //     }
+    //     printf("Elapsed time: %.2lf seconds\n", time);
+    // }
 
-    if (world_rank == 0) {
-        free(a);
-        free(b);
-        free(c);
-        MPI_Type_free(&array_block);
-    }
+    // if (world_rank == 0) {
+    //     free(a);
+    //     free(b);
+    //     free(c);
+    //     MPI_Type_free(&array_block);
+    // }
 
     MPI_Finalize();
     return 0;
